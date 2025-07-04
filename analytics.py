157e1e01dfae9be97bbd354f96259fbf444c9957
Raya Shahin <rayashahin08@gmail.com>
2025-07-04 23:15:27 +0300

    Add backup functionality

--- analytics.py
+++ analytics.py
@@ -2,4 +2,6 @@ def plot_expense_summary(db_path="data/expenses.db"):
     pass
 
 def backup_to_csv(db_path="data/expenses.db"):
-    pass
+    with sqlite3.connect(db_path) as conn:
+        data_frame = pd.read_sql_query("SELECT * FROM expenses", conn)
+        data_frame.to_csv("data/expenses_backup.csv", index=False)
