a718bad082507489ed89e662885de85b8acb75ef
Raya Shahin <rayashahin08@gmail.com>
2025-07-04 23:16:23 +0300

    Implement matplotlib plots functionality

--- analytics.py
+++ analytics.py
@@ -1,5 +1,29 @@
+import sqlite3
+import pandas as pd
+import matplotlib.pyplot as plt
+
 def plot_expense_summary(db_path="data/expenses.db"):
-    pass
+    with sqlite3.connect(db_path) as conn:
+        # retrieving the data from the database as a DataFrame
+        data_frame = pd.read_sql_query("SELECT * FROM expenses", conn)
+        
+    # checks if the DataFrame is empty or not 
+    if data_frame.empty:
+        print("No data to visualize.")
+        return
+    
+    # convert "date" column to datetime and extract month
+    data_frame["date"] = pd.to_datetime(data_frame["date"])
+    data_frame["month"] = data_frame["date"].dt.to_period("M") 
+    
+    # plotting the monthly expenses
+    monthly = data_frame.groupby("month")["amount"].sum()
+    monthly.plot(kind="bar", title="Monthly Expenses")
+    plt.xlabel("Month")
+    plt.ylabel("Total Amount")
+    plt.tight_layout()
+    plt.savefig("plots/monthly_expenses.png")
+    plt.show()
 
 def backup_to_csv(db_path="data/expenses.db"):
     with sqlite3.connect(db_path) as conn:
